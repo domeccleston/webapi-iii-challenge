390cca1c8f5afeb951dba01126112416bf8f3feb
Dom <domeccleston@gmail.com>
2019-10-30 13:14:36 +0000

    added logger

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-const express = 'express';
-
+const express = require('express');
 const server = express();
 
-server.get('/', (req, res) => {
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
-//custom middleware
+// custom middleware
 
+// logger(): logs request method, request url, and a timestamp. Runs on all requests to the API
 function logger(req, res, next) {
-
+  console.log(req.method);
+  console.log(req.url);
+  console.log(Date.now());
+  next();
 };
 
 module.exports = server;
